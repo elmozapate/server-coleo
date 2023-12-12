c1c0bd5fd7a8ebbfc8c71f6843687d444ed07ced
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-12 18:39:46 -0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,15 +1,12 @@
-const Variables = require("../models/variables")
 const SocketController = require("./controller")
 
-const VariablesC = Variables()
- const Soket = (socket, variables) => {
-    let { salas = VariablesC.salas, PromosArray = VariablesC.PromosArray, done = VariablesC.done, conection = VariablesC.conection, listenSocket = VariablesC.listenSocket, init = VariablesC.init, conectionCount = VariablesC.conectionCount, juegos = VariablesC.juegos, empresa = VariablesC.empresa, newHistorial = VariablesC.newHistorial, log = VariablesC.log, ecpData = VariablesC.ecpData, chat = VariablesC.chat, users = VariablesC.users, usuarios = VariablesC.usuarios, usuariosIn = VariablesC.usuariosIn } = (variables || VariablesC)
+ const Soket = (socket) => {
 
     socket.emit('coleoServer', {
         actionTodo: "serverOn"
     })
     socket.on('domino', (data) => {
-        SocketController( socket, variables, data)
+        SocketController( socket, data)
     })
     socket.on('disconnect', () => {
         console.log('disconnect', socket.id)
