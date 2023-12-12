80e57cc1f50dce2a4fac0d4dd6cc8b84f0e8da58
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-12 18:43:38 -0500

    Update basededatos.js

--- db/basededatos.js
+++ db/basededatos.js
@@ -5,10 +5,10 @@ try {
         const uri = "mongodb+srv://moet:moetzapata@cluster0.o52gvk2.mongodb.net/?retryWrites=true&w=majority";
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
         await client.connect().then(() => console.log('conectado a mongodb dataReq'))
-        let db = client.db('dominoDb')
+        let db = client.db('coleo')
         
         const usuariosF = async () => {
-            let dbUserRes = db.collection(task)
+            let dbUserRes = db.collection('usuarios')
             let result = dbUserRes.find({}).project({})
             let usuariosAux = await result.toArray()
             usuarios = usuariosAux
