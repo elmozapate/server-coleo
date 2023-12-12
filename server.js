14ce4cbb9aeec1e5dc671f0ce7fd92e53e08b35b
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-12 18:42:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const http = require("http").Server(app);
-
+let conectionCount= 0
 const Soket = require("./socket/socket.js");
 const Variables = require("./models/variables.js");
 const FdbRes = require("./funciones/FdbRes.js");
 let PORT = process.env.PORT || 3005;
 const VariablesC = Variables();
 
-let {
-  salas = VariablesC.salas,
-  PromosArray = VariablesC.PromosArray,
-  done = VariablesC.done,
-  conection = VariablesC.conection,
-  listenSocket = VariablesC.listenSocket,
-  init = VariablesC.init,
-  conectionCount = VariablesC.conectionCount,
-  juegos = VariablesC.juegos,
-  empresa = VariablesC.empresa,
-  newHistorial = VariablesC.newHistorial,
-  log = VariablesC.log,
-  ecpData = VariablesC.ecpData,
-  chat = VariablesC.chat,
-  users = VariablesC.users,
-  usuarios = VariablesC.usuarios,
-  usuariosIn = VariablesC.usuariosIn,
-} = VariablesC;
-let variablesAux = VariablesC;
 
 app.use(cors());
 http.listen(PORT, () => {
@@ -39,13 +20,11 @@ const io = require("socket.io")(http, {
   },
 });
 const recDb = async () => {
-  const respuesta = await FdbRes(variablesAux);
-    variablesAux = respuesta
+  const respuesta = true /*await FdbRes(variablesAux);*/
     io.on("connection", (socket) => {
-      variablesAux.conectionCount++;
       conectionCount++;
       console.log("User connection");
-      Soket(socket, variablesAux);
+      Soket(socket);
     });
 };
 recDb();
