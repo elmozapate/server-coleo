93f960de4f3c7cb32ee10eafb8aaa885db188b50
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-12 18:36:03 -0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -5,36 +5,13 @@ const VariablesC = Variables()
  const Soket = (socket, variables) => {
     let { salas = VariablesC.salas, PromosArray = VariablesC.PromosArray, done = VariablesC.done, conection = VariablesC.conection, listenSocket = VariablesC.listenSocket, init = VariablesC.init, conectionCount = VariablesC.conectionCount, juegos = VariablesC.juegos, empresa = VariablesC.empresa, newHistorial = VariablesC.newHistorial, log = VariablesC.log, ecpData = VariablesC.ecpData, chat = VariablesC.chat, users = VariablesC.users, usuarios = VariablesC.usuarios, usuariosIn = VariablesC.usuariosIn } = (variables || VariablesC)
 
-    socket.emit('dominoServer', {
+    socket.emit('coleoServer', {
         actionTodo: "serverOn"
     })
     socket.on('domino', (data) => {
         SocketController( socket, variables, data)
     })
     socket.on('disconnect', () => {
-        let posi = -1
-        let posiB = -1
-        let userD = ''
-        usuariosIn.map((key, i) => {
-            if (key.socket.id === socket.id) {
-                posi = i
-                userD = key.usuario || key.user
-
-            }
-        })
-        usuarios.map((key, i) => {
-            if ((key.usuario || key.user) === userD) {
-                posiB = i
-            }
-        })
-        posi !== -1 && usuariosIn.splice(posi, 1);
-        posiB !== -1 && usuarios.splice(posiB, 1);
-        socket.broadcast.emit('dominoServer', {
-            actionTodo: "usuarios",
-            usuarios: usuarios,
-            salas: salas,
-            chat: chat
-        })
         console.log('disconnect', socket.id)
     })
 }
