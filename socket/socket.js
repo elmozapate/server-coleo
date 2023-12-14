33dac4244ab117fc235ecf8bd77470bf6d5fdfa5
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-14 03:23:42 -0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -16,8 +16,8 @@ const SocketController = require("./controller")
                 onPos=i
             }
         });
-       ( usuariosIn[onPos].usuario && !usuariosIn[onPos].usuario.usuario ) && onPos&&usuariosIn.splice(onPos,1);
-       ( usuariosIn[onPos].usuario && !usuariosIn[onPos].usuario.usuario ) && onPos&&actUsuarios(usuariosIn);
+       ( usuariosIn[onPos].usuario && usuariosIn[onPos].usuario.usuario ) && onPos&&usuariosIn.splice(onPos,1);
+       ( usuariosIn[onPos].usuario && usuariosIn[onPos].usuario.usuario ) && onPos&&actUsuarios(usuariosIn);
     })
 }
 module.exports=Soket
