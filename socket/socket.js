95584f687cfd03e1e1c10ba358c7f70cb799e3ec
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-14 00:26:01 -0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,13 +1,13 @@
 const SocketController = require("./controller")
 
- const Soket = (socket,usuarios) => {
+ const Soket = (socket,usuarios,usuariosIn,actUsuarios) => {
 
   console.log(usuarios)
     socket.emit('coleoServer', {
         actionTodo: "serverOn"
     })
     socket.on('coleo', (data) => {
-        SocketController( socket, data)
+        SocketController( socket, data,usuariosIn,actUsuarios)
     })
     socket.on('disconnect', () => {
         console.log('disconnect', socket.id)
