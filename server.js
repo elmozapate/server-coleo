5e34ee44dee916524530a83738c0496c9cc4fbac
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-15 00:24:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,41 +22,6 @@ const io = require("socket.io")(http, {
 });
 const recDb = async () => {
   const respuesta = await Basededatos()
-  if(respuesta){
-    let manyH = respuesta.length
-    const mandarNuevU=async(veces)=>{
-      if(veces>0){
-         let newUser={
-           validate: false,
-           contactado: false,
-           ip: 0,
-           email:'',
-           id: 0,
-           conectado: false,
-           admin: false,
-           telefono:0,
-           acceso:{
-             dias:[],
-           },
-           dias:{
-             jueves:false,
-             viernes:false,
-             sabado:false,
-             domingo:false
-           },
-           amin:false,
-           conecciones:1,
-           pago:false
-         }
-        const vez=veces - 1
-        const req= await DbPut({value:respuesta[vez],coleccion:'usuarios'})
-        if(req){
-          mandarNuevU(vez)
-        }
-      }
-    }
-    mandarNuevU(manyH)
-  }
   io.on("connection", (socket) => {
       conectionCount++;
       console.log("User connection");
